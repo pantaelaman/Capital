0f96c363a02595091e5e1a04c6c208966220bb27
pantaelaman <calvin@theophoffs.com>
2022-05-17 17:50:35 -0400

    Added ducking

--- components/scripts/Player.gd
+++ components/scripts/Player.gd
@@ -8,6 +8,7 @@ var velocity = Vector2()
 
 var moving = false
 var arial = false
+var ducking = false
 
 var jump_velocity = 0
 var speed_modifier = 1
@@ -27,30 +28,42 @@ func _physics_process(delta):
 			speed_modifier = 1
 	
 	moving = false
-	if Input.is_action_pressed("act_right"):
+	if Input.is_action_pressed("act_right") && !ducking:
 		velocity.x += SPEED * 1000 * speed_modifier
 		moving = true
 		$view.flip_h = false
-	if Input.is_action_pressed("act_left"):
+	if Input.is_action_pressed("act_left") && !ducking:
 		velocity.x -= SPEED * 1000 * speed_modifier
 		moving = true
 		$view.flip_h = true
 	
-	if Input.is_action_just_pressed("act_up") && is_on_floor():
+	if Input.is_action_just_pressed("act_up") && is_on_floor() && !ducking:
 		jump_velocity -= JUMP_SPEED * 1000
+	
+	if Input.is_action_pressed("act_down") && is_on_floor() && !ducking:
+		ducking = true
+		$collision_normal.disabled = true
+		$collision_ducking.disabled = false
+	
+	if Input.is_action_just_released("act_down"):
+		ducking = false
+		$collision_normal.disabled = false
+		$collision_ducking.disabled = true
 	
 	jump_velocity = lerp(jump_velocity, 0, 0.04)
 	
 	arial = !is_on_floor()
 	
 	velocity.y += jump_velocity
-	print(velocity.y)
 	
 	handle_view()
 	move_and_slide(velocity * delta, Vector2.UP)
 	
 
 func handle_view():
+	if ducking:
+		$view.animation = 'ducking'
+		return
 	if arial:
 		$view.animation = 'arial'
 		return
